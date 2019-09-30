51d34c4216103a1159e1300641720fa772ff70fd
Okwudili Pat-Nebe <cp.nebe@gmail.com>
2019-09-30 20:13:13 +0100

    First Upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
+})
+
+export class AppComponent {
+  // tslint:disable-next-line: no-inferrable-types
+  pageTitle: string = 'Nebe Angular';
+}
